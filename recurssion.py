91a81e78c9d46bae16185674b74e0430e48012cb
Aadhar <chandiwalaaadhar@gmail.com>
2020-07-18 16:03:50 +0530

    Added Check Palindrome through Recurssion

--- recurssion.py
+++ recurssion.py
@@ -3,7 +3,7 @@ def tower_of_hanoi(no_of_disks, source_rod=1, destination_rod=3):
     if no_of_disks==0:
         return
     else:
-        tower_of_hanoi(no_of_disks-1, source_rod, 6-source_rod-destination_rod) #Recurcive Leap of Faith
+        tower_of_hanoi(no_of_disks-1, source_rod, 6-source_rod-destination_rod) #Recurssive Leap of Faith
         print("move disk number {} from tower {} to tower {}".format(no_of_disks, source_rod, destination_rod))
         tower_of_hanoi(no_of_disks-1, 6-source_rod-destination_rod, destination_rod)
 
@@ -14,3 +14,13 @@ def digit_to_bitstr(num):
     if (num==1):
         return ['0','1']
     return [digit+bitstr for digit in digit_to_bitstr(1) for bitstr in digit_to_bitstr(num-1)]
+
+def check_palindrome(word):
+    '''Checking Palindrome Number using Recurssion'''
+    if (len(word)<=1): #base case 1
+        return True
+    elif (len(word)==2 and word[0]==word[1]): #base case 2
+        return True
+    elif (word[0]==word[-1] and check_palindrome(word[1:-1]) is True): #Recurssive Leap of Faith
+        return True
+    return False
