ef039f027230a51bb0f4fa254cefc98bda8fcfd3
Aadhar <chandiwalaaadhar@gmail.com>
2020-07-21 12:21:06 +0530

    Created linkedlist.py and Created a Node Class

---
+++ linked_lists.py
@@ -0,0 +1,5 @@
+# Create a Node Class that has charateristics of a Node of LL
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data=data  # This is the variable where data is stored
+        self.next=next  # This is the variable in which pointer to the next node is stored
